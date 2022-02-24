999bd3fb8979ca75a3f7e47f49e07a77e3904d65
sgibbs409 <asdfasd@asdfad.com>
2022-02-24 11:32:30 -0800

    Added ability to run arbitrary commands

---
+++ main.go
@@ -0,0 +1,36 @@
+// docker			run image <cmd> <params>
+// go run main.go   run       <cmd> <params>
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+
+	default:
+		panic("bad command")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
